5d3c72225ac222e8c7d18f32a8d6187ca50c767b
pkadlubowski <p.kadlubowski@madkom.pl>
2020-01-30 13:49:57 +0100

    cleanup and timer

--- src/components/Home/HomeView.js
+++ src/components/Home/HomeView.js
@@ -1,23 +1,57 @@
 import React from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 import HomeViewStyles from './HomeViewStyles';
+import i18n from '../../i18n/i18n';
 
-const HomeView = () => {
-  return (
-    <View style={[{flex: 1}, HomeViewStyles.homeViewContainer]}>
-      <View style={{flex: 1}}>
-        <Text style={HomeViewStyles.welcomeHeader}>Good Morning!</Text>
-      </View>
+class HomeView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+    };
+  }
 
-      <View style={{flex: 2}}>
-        <TouchableOpacity
-          style={HomeViewStyles.mainActionButton}
-          onPress={console.log('button pressed')}>
-          <Text style={HomeViewStyles.mainActionButtonText}>Start </Text>
-        </TouchableOpacity>
+  renderStartButton = () => {
+    return (
+      <TouchableOpacity
+        style={HomeViewStyles.mainActionButton}
+        onPress={() => {
+          setInterval(() => {
+            this.setState({time: this.state.time + 1000});
+          }, 1000);
+        }}>
+        <Text style={HomeViewStyles.mainActionButtonText}>
+          {i18n.HOME.START}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  renderRunningTimer = () => {
+    const {time} = this.state;
+    return (
+      <TouchableOpacity style={HomeViewStyles.mainActionButton}>
+        <Text style={HomeViewStyles.mainActionButtonText}>{time}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  render() {
+    const {time} = this.state;
+    return (
+      <View style={[{flex: 1}, HomeViewStyles.homeViewContainer]}>
+        <View style={{flex: 1}}>
+          <Text style={HomeViewStyles.welcomeHeader}>
+            {i18n.HOME.WELCOME_HEADER}
+          </Text>
+        </View>
+
+        <View style={{flex: 2}}>
+          {time === 0 ? this.renderStartButton() : this.renderRunningTimer()}
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 export default HomeView;
